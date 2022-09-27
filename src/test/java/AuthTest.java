92ef7648f7fbd86b3e5c15e0b5b843c082329647
Kseniya <ryabova.ksyusha@gmail.com>
2022-09-27 19:48:17 +0300

    added creating and deliting user for test

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,4 +1,11 @@
+import dto.CreateAndAuthUserResponse;
+import dto.User;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class AuthTest extends BaseTest {
 
@@ -8,33 +15,57 @@ public class AuthTest extends BaseTest {
     HeaderElement headerElement = new HeaderElement();
     ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
 
-    String validEmail = "ryabova.ksyusha@gmail.com";
-    String validPassword = "111111Qw";
-    String invalidPassword = "111111Q";
+    public static final String email = ((int)(Math.random()*10000))+"troyan1@gmail.com" + (int)(Math.random()*10000);
+    public static final String password = "123456";
+    public static final String invalidPassword = "123";
+    public static final String name = "ksyusha";
+    public static String tokenUser;
+
+    @BeforeClass
+    public static void createUser() {
+        User user = new User(email, password, name);
+        CreateAndAuthUserResponse responseUser =
+                given().spec(specification)
+                        .body(user)
+                        .when()
+                        .post("/api/auth/register")
+                        .body().as(CreateAndAuthUserResponse.class);
+        tokenUser = responseUser.getAccessToken();
+    }
+
+    @AfterClass
+    public static void deleteUser() {
+        given().spec(specification)
+                .header("Authorization", tokenUser)
+                .when()
+                .delete("api/auth/user")
+                .then()
+                .statusCode(202);
+    }
 
     @Test
     public void authWithValidData() {
-        authPage.auth(validEmail, validPassword);
+        authPage.auth(email, password);
         mainPage.checkTitle();
     }
 
     @Test
     public void authWithInvalidData() {
-        authPage.auth(validEmail, invalidPassword)
+        authPage.auth(email, invalidPassword)
                 .checkOutputIncorrectPasswordError();
     }
 
     @Test
     public void authInMainPage() {
         mainPage.open().clickSingInAccountButton();
-        authPage.auth(validEmail, validPassword);
+        authPage.auth(email, invalidPassword);
         mainPage.checkTitle();
     }
 
     @Test
     public void authInRegisterPage() {
         registerPage.open().clickSingIn();
-        authPage.auth(validEmail, validPassword);
+        authPage.auth(email, invalidPassword);
         mainPage.checkTitle();
     }
 
@@ -42,15 +73,14 @@ public class AuthTest extends BaseTest {
     public void authInAccount() {
         headerElement.open()
                 .clickAccountButton();
-        authPage.auth(validEmail, validPassword);
+        authPage.auth(email, invalidPassword);
         mainPage.checkTitle();
     }
 
     @Test
     public void authInForgotPasswordPage() {
-        forgotPasswordPage.open().
-                clickToLinkSingIn();
-        authPage.auth(validEmail, validPassword);
+        forgotPasswordPage.open().clickToLinkSingIn();
+        authPage.auth(email, invalidPassword);
         mainPage.checkTitle();
     }
 }
