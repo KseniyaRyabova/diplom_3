f9d59b3c5b337ab2b558d5c3b4521796baa5cf0c
Kseniya <ryabova.ksyusha@gmail.com>
2022-09-27 20:06:47 +0300

    added deleting user after register test

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -14,7 +14,24 @@ public class RegisterTest extends BaseTest {
     public static final String password = "123456";
     public static final String invalidPassword = "123";
     public static final String name = "ksyusha" + (int)(Math.random()*10000);
+    public static String tokenUser;
 
+    @AfterClass
+    public static void deleteUser() {
+        User user = new User(email, password);
+        CreateAndAuthUserResponse response = given().spec(specification)
+                .body(user)
+                .when()
+                .post("/api/auth/login").as(CreateAndAuthUserResponse.class);
+        tokenUser = response.getAccessToken();
+
+        given().spec(specification)
+                .header("Authorization", tokenUser)
+                .when()
+                .delete("api/auth/user")
+                .then()
+                .statusCode(202);
+    }
     @Test
     public void registerWithValidData() {
         registerPage.open()
