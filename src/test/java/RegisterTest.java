9f40b0822ce310cc45f2ca77603b6d3224b0459b
Kseniya <ryabova.ksyusha@gmail.com>
2022-09-27 19:59:27 +0300

    added random data for Register tests

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,15 +1,26 @@
+import dto.CreateAndAuthUserResponse;
+import dto.User;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class RegisterTest extends BaseTest {
     AuthPage authPage = new AuthPage();
     RegisterPage registerPage = new RegisterPage();
 
+    public static final String email = ((int)(Math.random()*10000))+"troyan1@gmail.com" + (int)(Math.random()*10000);
+    public static final String password = "123456";
+    public static final String invalidPassword = "123";
+    public static final String name = "ksyusha" + (int)(Math.random()*10000);
+
     @Test
     public void registerWithValidData() {
         registerPage.open()
-                .enterName("Name")
-                .enterEmail("ryab@ka.com")
-                .enterPassword("111111Qw")
+                .enterName(name)
+                .enterEmail(email)
+                .enterPassword(password)
                 .clickSingUpButton();
         authPage.checkTitle();
     }
@@ -17,9 +28,9 @@ public class RegisterTest extends BaseTest {
     @Test
     public void registerWithIncorrectPassword() {
         registerPage.open()
-                .enterName("Name")
-                .enterEmail("ryab@ka.com")
-                .enterPassword("111")
+                .enterName(name)
+                .enterEmail(email)
+                .enterPassword(invalidPassword)
                 .clickSingUpButton()
                 .checkErrorUnderPasswordInput();
     }
